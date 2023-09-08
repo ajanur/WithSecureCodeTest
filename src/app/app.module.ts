eb75c56e5bf9942ed49fe00a22db7fc7928b4f6c
ajanur <64955170+ajanur@users.noreply.github.com>
2023-09-08 15:46:01 +0100

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoListComponent } from './src/todo-list/todo-list.component';
 import { PageNotFoundComponent } from './src/page-not-found/page-not-found.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { GlobalErrorHandlerService } from './services/global-error-handler.service';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
-import { TestComponent } from './src/test/test.component';
 import { ProductListComponent } from './src/product-list/product-list.component';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
@@ -22,10 +19,8 @@ import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent,
     PageNotFoundComponent,
     ProductListComponent,
-    TestComponent
   ],
   imports: [
     BrowserModule,
--- src/app/models/enum.model.ts
+++
@@ -1,6 +0,0 @@
-export enum Status {
-    Created = 0,
-    InProgress = 1,
-    Completed = 2
-  }
-  
